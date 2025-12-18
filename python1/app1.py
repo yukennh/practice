f5a5458f3ce503341ebcdd606bde30319327ea05
yukennh <yukennh@gmail.com>
2025-12-18 02:17:42 +0000

    dec 18 sql and python practice

--- python1/app1.py
+++ python1/app1.py
@@ -34,6 +34,33 @@ print("---------")
 capitals.pop("China")
 print(capitals)
 print("---------")
-# sample of clearing the dictionary
-capitals.clear()
+# sample of removing the latest set of pair
+capitals.popitem()
+print(capitals)
 print("---------")
+# sample of clearing the dictionary
+# capitals.clear()
+# print("---------")
+# sample of getting all the keys using method and setting it as variable
+keys = capitals.keys()
+print(keys)
+print("---------")
+# sample of for loop to show all the key in each row
+for key in capitals.keys():
+    print(key)
+print("---------")
+# sample of getting all the values using method and setting it as variable
+values = capitals.values()
+print(values)
+print("---------")
+# sample of for loop to show all the value in each row
+for value in capitals.values():
+    print(value)
+print("---------")
+# sample of for loop to show all the key value pairs in each row
+for key, value in capitals.items():
+    print(f"{key} : {value}")
+print("---------")
+
+for key, value in capitals.items():
+    print(f"INSERT INTO capitals (country, capital) VALUES ('{key}','{value}');")
---
+++ python1/app2.py
@@ -0,0 +1,58 @@
+
+# add
+alpha = 0
+alpha = alpha + 1
+print(f"alpha: {alpha}")
+# another way to add
+bravo = 0
+bravo += 1
+print(f"bravo: {bravo}")
+print("-----\n")
+
+# subtract
+charlie = 10
+charlie = charlie - 1
+print(f"charlie: {charlie}")
+# or another way to subtract
+delta = 10
+delta -= 1
+print(f"delta: {delta}")
+print("-----\n")
+
+# multiply
+echo = 4
+echo = echo * 5
+print(f"echo: {echo}")
+# or another way to subtract
+foxtrot = 4
+foxtrot *= 5
+print(f"foxtrot: {foxtrot}")
+print("-----\n")
+
+# divide
+gamma = 50
+gamma = gamma / 2
+print(f"gamma: {gamma}")
+# or another way to divide
+harry = 50
+harry /= 2
+print(f"harry: {harry}")
+print("-----\n")
+
+# exponent
+igloo = 10
+igloo = igloo ** 2
+print(f"igloo: {igloo}")
+john = 10
+john **= 2
+print(f"john: {john}")
+print("-----\n")
+
+# get the remainder
+kilo = 10
+lima = 3
+remainder = kilo % lima
+print(f"the remainder of {kilo} divided by {lima} is = {remainder}")
+
+# or
+# or
