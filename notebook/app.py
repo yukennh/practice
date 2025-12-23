c180fd8a3db53ae1095a2eada688246687f2816b
yukennh <yukennh@gmail.com>
2025-12-23 04:11:07 +0000

    dec 23 & 24 python and sql

--- notebook/app.py
+++ notebook/app.py
@@ -6,8 +6,21 @@ cursor.execute("SELECT sql FROM sqlite_master WHERE type='table' AND name='notes
 schema = cursor.fetchone()
 print(schema[0])
 
-date = str(input("Date: "))
+day = int(input("What is the day today? "))
+while day < 1 or day > 31:
+    print("Enter a valid day from 1-31.")
+    day = int(input("What is the day today? "))
+
+month = int(input("What is the month today? "))
+while month < 1 or month > 12:
+    print("Enter a valid month from 1-12.")
+    month = int(input("What is the month today? "))
+
+date = str(f"{day}/{month}/2025")
 note = str(input("Enter your notes here: "))
+while note == "":
+    print("Your notes cannot be blank.")
+    note = str(input("Enter your notes here: "))
 
 #instead of using (f"INSERT INTO notes (date, notes) VALUES ('{date}','{note}');") as an SQL, the one below is used to avoid sql inject
 cursor.execute("INSERT INTO notes (date, notes) VALUES (?,?)",(date, note))
---
+++ python1/app6.py
@@ -0,0 +1,37 @@
+#sample of for loop
+for x in range (1,11):
+    print(x*2)
+
+#sample of for loop for a list
+fruits = ["apple","banana","coconut","dalandan"]
+for fruit in fruits:
+    print(fruit)
+
+#sample of while loop
+name = input("Enter your name: ")
+while name == "":
+    print("Please enter your name.")
+    name = input("Enter your name: ") #make sure that you have in a loop a statement to avoid infinite loop
+print(f"Hello {name}!")
+
+#sample of while loop
+age = int(input("Enter your age: "))
+while age < 0:
+    print("Please enter a valid age")
+    age = int(input("Enter your age: "))
+print(f"You are {age} years old {name}")
+
+#sample of while not loop
+idea = input("What are you thinking now (q to quit) ?")
+while not idea == "q":
+    print(f"{name}'s idea is: {idea}")
+    idea = input("What are you thinking now (q to quit) ?")
+print(f"Got it {name}!")
+
+#sample of while loop with OR
+happiness = int(input("From 1 to 10, 10 being the highest, how happy are you today?"))
+while happiness < 1 or happiness > 10:
+    print("Not a valid answer")
+    happiness = int(input("From 1 to 10, 10 being the highest, how happy are you today?"))
+print(f"{name}, {age} years old has a lot of ideas. His happiness level is at {happiness}. Have a great day!")
+
