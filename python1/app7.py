6a5336aa158379838f2a81b9e31f834c45874e68
yukennh <yukennh@gmail.com>
2025-12-25 12:12:57 +0800

    dec 25 - sql and python practice

--- python1/app7.py
+++ python1/app7.py
@@ -3,10 +3,11 @@ def happy_birthday(name, age): #name and age are arguments here. you can think o
     print(f"happy birthday {name}!") #this is an action in the function where it uses one of the arguments
     print(f"you are {age} years old!")
 
-#sample of invoking / executing a function with the two arguments designed
+#sample of invoking / executing a function with the two arguments assigned
 happy_birthday("John",40)
 happy_birthday("Ryan",13)
 
+#sample of assigning a variable and using that variable as parameter to the argument needed in the function
 first_name = str(input("First Name: "))
 last_name = str(input("Last Name: "))
 
@@ -14,3 +15,16 @@ def register(firstname, lastname):
     print(f"hi {firstname} {lastname}!")
 
 register(first_name,last_name)
+
+#sample of definining a function with return functionality where the parameter of it's argument are first defined
+department = str(input("What is your department? "))
+team = str(input("What is your team's name? "))
+
+def group(d,t):
+    d = d.capitalize()
+    t = t.capitalize()
+    return t + " | " + d
+
+orgplace = group(department, team)
+
+print(orgplace)
---
+++ sql1/app.py
@@ -0,0 +1,9 @@
+import sqlite3
+conn = sqlite3.connect("school.db")
+cursor = conn.cursor()
+
+#define action
+    #register a subject
+    #register a teacher
+    #create a class
+    
