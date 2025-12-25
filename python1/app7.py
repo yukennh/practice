405a9c341d80528b6e47137fd256c0b4eae4ba89
yukennh <yukennh@gmail.com>
2025-12-25 11:42:14 +0800

    Create app7.py

---
+++ python1/app7.py
@@ -0,0 +1,16 @@
+#sample of defining or creating a function
+def happy_birthday(name, age): #name and age are arguments here. you can think of it as a variable used within the function
+    print(f"happy birthday {name}!") #this is an action in the function where it uses one of the arguments
+    print(f"you are {age} years old!")
+
+#sample of invoking / executing a function with the two arguments designed
+happy_birthday("John",40)
+happy_birthday("Ryan",13)
+
+first_name = str(input("First Name: "))
+last_name = str(input("Last Name: "))
+
+def register(firstname, lastname):
+    print(f"hi {firstname} {lastname}!")
+
+register(first_name,last_name)
