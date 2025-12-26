f9a126ba7a1165716da8917e33bc0dde6318392a
yukennh <yukennh@gmail.com>
2025-12-26 13:51:58 +0800

    dec 26 python and sql practice

--- sql1/app.py
+++ sql1/app.py
@@ -12,11 +12,30 @@ while action not in ['s','t','c']:
     action = str(input("What do you want to do?"))
 
 if action == 's':
-    print("proceed with the creation of subject")
+    print("Provide subject details.")
+    subj_name = str(input("Subject Name: "))
+    subj_code = str(input("Subject Code: "))
+    subj_credits = float(input("Subject Credits: "))
+    
+    subj_confirm = bool(input(f"Confirm Changes\nSubject Name:{subj_name} \nSubject Code:{subj_name} \nSubject Credits:{subj_credits} \n(y/n)?"))
+        
+    print(f"INSERT INTO subjects (subject_name, subject_code, credits) VALUES({subj_name},{subj_code},{subj_credits})")
+
 elif action == 't':
-    print("proceed with registration of teacher")
+    print("Provide teacher's details.")
+    t_fname = str(input("First Name: "))
+    t_lname = str(input("Last Name: "))
+    t_email = str(input("Email Address: "))
+    t_department = str(input("Department: "))
+    print(f"INSERT INTO teachers (first_name,last_name,email,department) VALUES({t_fname},{t_lname},{t_email},{t_department})")
 elif action == 'c':
-    print("proceed with the creation of class")
+    print("Provide class details.")
+    print("Subject ID: ")
+    print("Teacher ID: ")
+    print("Semester: ")
+    print("Schedule: ")
+    print("Room: ")
+    
 
 #define action
     #create a subject
